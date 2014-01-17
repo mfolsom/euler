e4540be28143891cd5387163c4fd9551c50510ef
Megan Folsom <mfolsom@gmail.com>
2014-01-17 09:55:56 +0000

    refactored filter

--- euler.rb
+++ euler.rb
@@ -1,20 +1,12 @@
 
 def filter_numbers
-new_items = []
-
-	(1..999).each do |i|
-   		new_items << i if (i % 3 ==0 || i % 5 == 0)
-   		unless i.nil?
-		end
-
-	end
-
-output = new_items
-
+	new_items = []
+	(1..999).map {|i| new_items << i if (i % 3 ==0 || i % 5 == 0)}
+	output = new_items.compact
 end
 
 def euler
 	filter_numbers.inject(0) { |sum, num| sum + num}
-
 end
 
+#puts euler
