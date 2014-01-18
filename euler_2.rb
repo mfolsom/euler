e01e11f039aaf7f0611197a47680428331e09348
Megan Folsom <mfolsom@gmail.com>
2014-01-18 13:42:34 +0000

    added skeleton test and first fib filter

---
+++ euler_2.rb
@@ -0,0 +1,15 @@
+
+#def fibonacci
+
+fib = Enumerator.new do |y|
+  i = 0
+  j = 1
+  loop  do
+    i=j+j=i
+    y.yield i 
+end
+end
+
+#end
+
+puts fib.next.next.next
---
+++ spec/euler_2_spec.rb
@@ -0,0 +1,9 @@
+require './euler_2.rb'
+
+describe "Euler Problem 2" do 
+
+it "something here" do
+
+end
+
+end
